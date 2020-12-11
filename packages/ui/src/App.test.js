98527c720dc62cd8e1b377f461a01d62157a5fa5
Nick Lucas <me@nicklucas.co.uk>
2020-12-11 18:10:13 +0000

    Fix tests

--- packages/ui/src/App.test.js
+++ packages/ui/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
-import App from './App'
+import { App } from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/Dashboard/i)
+  const linkElement = screen.getAllByText(/Dashboard/i)[0]
   expect(linkElement).toBeInTheDocument()
 })
