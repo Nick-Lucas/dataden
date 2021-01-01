d664ea5eb0d58a4dcc2c8c793d3f088632946354
Nick Lucas <me@nicklucas.co.uk>
2021-01-01 23:12:12 +0000

    Get build script working

--- packages/sdk/configs/rollup.plugin.config.js
+++ packages/sdk/configs/rollup.plugin.config.js
@@ -1,13 +1,14 @@
 import typescript from '@wessberg/rollup-plugin-ts'
 import commonjs from '@rollup/plugin-commonjs'
 
-/** @type {import('rollup').RollupOptions} */
+/** @type {() => import('rollup').RollupOptions} */
 export default () => ({
   input: 'src/index.ts',
   output: {
     dir: 'dist',
     format: 'cjs',
-    sourcemap: true
+    sourcemap: true,
+    exports: 'auto'
   },
   plugins: [
     typescript({
