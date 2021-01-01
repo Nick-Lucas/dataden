e26b897650262143e36e41111d4fefaa34b2eb9c
Nick Lucas <me@nicklucas.co.uk>
2021-01-01 23:51:24 +0000

    Correct the output directory of ts declarations

--- packages/sdk/configs/rollup.plugin.config.js
+++ packages/sdk/configs/rollup.plugin.config.js
@@ -1,27 +1,33 @@
 import typescript from '@wessberg/rollup-plugin-ts'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import json from '@rollup/plugin-json'
+
+import tsConfig from './tsconfig.plugin.json'
 
 /** @type {() => import('rollup').RollupOptions} */
-export default () => ({
-  input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'cjs',
-    sourcemap: true,
-    exports: 'auto'
-  },
-  plugins: [
-    typescript({
-      tsconfig: __dirname + '/tsconfig.plugin.json',
-      exclude: ['node_modules/**/*', '*/**/node_modules/**/*']
-    }),
-    resolve(),
-    commonjs({
-      include: /node_modules/
-    })
-  ],
+export default () => {
+  return {
+    input: 'src/index.ts',
+    output: {
+      dir: 'dist',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'auto'
+    },
+    plugins: [
+      typescript({
+        tsconfig: tsConfig.compilerOptions,
+        exclude: ['node_modules/**/*', '*/**/node_modules/**/*']
+      }),
+      resolve(),
+      commonjs({
+        include: /node_modules/
+      }),
+      json()
+    ],
 
-  // Important when using a `yarn link` version of the package, while deveoping the sdk against a plugin
-  preserveSymlinks: true
-})
+    // Important when using a `yarn link` version of the package, while deveoping the sdk against a plugin
+    preserveSymlinks: true
+  }
+}
