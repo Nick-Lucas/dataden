d5295635d389bde03fd7c9f63129c8dc9cc4f452
Nick Lucas <me@nicklucas.co.uk>
2021-01-13 20:32:13 +0000

    WIP replace UI project

--- packages/ui/src/app.test.tsx
+++ packages/ui/src/app.test.tsx
@@ -1,22 +1,9 @@
-import { ReactNode } from 'react'
-import { render, screen } from '@testing-library/react'
-import { App } from './App'
-
-import { QueryClientProvider, QueryClient } from 'react-query'
-
-const queryClient = new QueryClient()
-function Wrapper({ children }: { children: ReactNode }) {
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  )
-}
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
 test('renders learn react link', () => {
-  render(
-    <Wrapper>
-      <App />
-    </Wrapper>
-  )
-  const linkElement = screen.getAllByText(/Dashboard/i)[0]
-  expect(linkElement).toBeInTheDocument()
-})
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
--- packages/ui_old/config/webpack.config.js
+++ packages/ui_old/config/webpack.config.js
@@ -398,7 +398,7 @@ module.exports = function (webpackEnv) {
                   [
                     require.resolve('babel-preset-react-app'),
                     {
-                      runtime: hasJsxRuntime ? 'automatic' : 'classic'
+                      runtime: 'automatic' //hasJsxRuntime ? 'automatic' : 'classic'
                     }
                   ]
                 ],
