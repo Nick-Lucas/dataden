d25c30739834542eda68afe63fd26186c57ce2f6
Nick Lucas <me@nicklucas.co.uk>
2021-02-07 12:56:41 +0000

    Add test database and move getClient into its own file

--- packages/core/src/db/common.ts
+++ packages/core/src/db/common.ts
@@ -1,6 +1,3 @@
-import { MongoClient } from 'mongodb'
-import { getConfig } from 'src/config'
-
 export const DATABASES = {
   CORE: 'core',
   PLUGIN_PREFIX: 'plugin__',
@@ -12,17 +9,6 @@ export const COLLECTIONS = {
     PLUGINS: 'plugins',
     USERS: 'users'
   }
-}
-
-let _client = null
-export async function getClient() {
-  if (!_client) {
-    _client = await MongoClient.connect(getConfig().MONGO_URI, {
-      useUnifiedTopology: true
-    })
-  }
-
-  return _client
 }
 
 export interface PagingPosition {
