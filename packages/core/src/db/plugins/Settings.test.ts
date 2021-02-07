f35cbc577bcda91d23a48a4a58039b501ad4cae5
Nick Lucas <me@nicklucas.co.uk>
2021-02-07 15:12:37 +0000

    Add tests again Settings access

---
+++ packages/core/src/db/plugins/Settings.test.ts
@@ -0,0 +1,46 @@
+import { MongoClient } from 'mongodb'
+import { getClient, getClientMocked } from 'src/db/getClient'
+import { Settings } from './Settings'
+import type { Settings as ISettings } from './Settings'
+import { DbPath } from './types'
+
+describe('DB: Syncs', () => {
+  let client: MongoClient
+  const dbPath: DbPath = { pluginId: 'my-plugin', instanceName: 'my-instance' }
+
+  beforeEach(async () => {
+    client = await getClient()
+
+    expect(getClientMocked).toBe(true)
+  })
+
+  afterEach(async () => {
+    await client.close(true)
+    client = null
+  })
+
+  it('returns default settings values on first read', async () => {
+    const settings = await Settings.get(client, dbPath)
+    expect(settings).toEqual(null)
+  })
+
+  it('sets settings', async () => {
+    const newSettings: ISettings = {
+      plugin: {
+        plugin: 'setting'
+      },
+      schedule: {
+        every: 1,
+        grain: 'hour'
+      },
+      secrets: {
+        plugin: 'secret'
+      }
+    }
+
+    await Settings.set(client, dbPath, newSettings)
+
+    const settings = await Settings.get(client, dbPath)
+    expect(settings).toEqual(newSettings)
+  })
+})
